f86a4850740ddccd98f93126207642b3feb381fd
Oleg Markoff <mawglih@gmail.com>
2019-04-14 18:50:01 -0400

    update

--- index.js
+++ index.js
@@ -3,15 +3,30 @@ const cors = require('cors');
 const path = require('path');
 const bodyParser = require('body-parser');
 const items = require('./items');
-const websites = require('./websites');
+
 
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const port = 8080;
-const transporter = require('./Mailer/transport');
 
-app.use(cors());
+//cors
+const allowedOrigins = [
+  'http://localhost:3000',
+];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -27,56 +42,10 @@ io.on('connection', (client) => {
 app.get('/get-items', (req, res) => {
 	res.json(items);
 });
-app.get('/get-websitess', (req, res) => {
-	res.json(websites);
-});
 
-app.post('/checkout', (req, res) => {
-	const hasError = !req.body.items;
-	let response;
 
-	if (hasError) {
-		response = {
-			status: 'error',
-			error: 'Invalid request.',
-		};
-	} else {
-		response = {
-			status: 'success',
-			message: 'Your order was placed successfully.',
-		};
-	}
+app.listen(port, () => console.log(`Server running on port ${port}`))
+console.log(`server is ready on port ${port}`)
 
-	res.json(response);
-});
-
-app.post('/send', (req, res, next) => {
-  var name = req.body.name
-  var email = req.body.email
-  var message = req.body.message
-  var content = `name: ${name} \n email: ${email} \n message: ${content} `
-
-  var mail = {
-    from: name,
-    to: 'mawglih@gmail.com', 
-    subject: 'New Message from Contact Form',
-    text: content
-  }
-
-  transporter.sendMail(mail, (err, data) => {
-    if (err) {
-      res.json({
-        msg: 'fail'
-      })
-    } else {
-      res.json({
-        msg: 'success'
-      })
-    }
-  })
-});
-// app.listen(port, () => console.log(`Server running on port ${port}`))
-// console.log(`server is ready on port ${port}`)
-
-io.listen(port);
-console.log(`socket is listening ${port}!`);
+// io.listen(port);
+// console.log(`socket is listening ${port}!`);
